30d1a1e4aa82bfa6631c298e0ed571f4d28a6c20
dlopez83 <43835133+dlopez83@users.noreply.github.com>
2018-10-15 18:25:37 -0600

    Update InClass.py

--- InClass.py
+++ InClass.py
@@ -1,6 +1,14 @@
+def float_to_int(float_list):
+	int_list = []
+	for f in float_list:
+		int_list.append(int(f))
+	return int_list
+
 if __name__ == "__main__":
-	floats = [float(i) for i in range(100)]
-	floats = []
-	for i in range(100):
-		floats.append(float(i))
-	print(floats)
+	 floats = [float(i) for i in range(100)]
+	 print(float_to_int(floats))
+	# floats = []
+	# for i in range(100):
+	# 	floats.append(float(i))
+	
+	#print(floats)
