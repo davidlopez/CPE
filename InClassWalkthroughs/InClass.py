c788f4228bc66a23213eb4701e27e59741f5c72c
dlopez83 <43835133+dlopez83@users.noreply.github.com>
2018-10-15 19:10:34 -0600

    Update InClassWalkthroughs/InClass.py

--- InClassWalkthroughs/InClass.py
+++ InClassWalkthroughs/InClass.py
@@ -3,10 +3,26 @@ def float_to_int(float_list):
 	for f in float_list:
 		int_list.append(int(f))
 	return int_list
+	
+def string_sandbox(string_list):
+	cap_list = []
+	for s in string_list:
+		cap_list.append(s[:1].upper() + s[1:])
+	return " ".join(cap_list)
+
 
 if __name__ == "__main__":
 	 floats = [float(i) for i in range(100)]
-	 print(float_to_int(floats))
+	 ints = float_to_int(floats)
+	 print(ints)
+	 strings = ['s' + str(f) for f in floats]
+	 print(strings)
+	 print(string_sandbox(strings))
+	 # which =
+	 # strings = []
+	 # for f in floats:
+	 # 	string.append('s' + str(f))
+	# print(float_to_int(floats))
 	# floats = []
 	# for i in range(100):
 	# 	floats.append(float(i))
